9448432ca8db23319936ba986482ea3fc459bac7
sigegea <17645026462@163.com>
2020-10-09 11:45:51 +0800

    双环境配置

--- src/api/http.js
+++ src/api/http.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
+var root = process.env.VUE_APP_URL
 var instance = axios.create({
     headers: {
         'Content-Type':'application/json'
     },
-    baseURL:'http://stuq.ceshiren.com:8089/'
+    // baseURL:'http://stuq.ceshiren.com:8089/'
+    baseURL: root
 })
 
 instance.interceptors.request.use(config=>{
